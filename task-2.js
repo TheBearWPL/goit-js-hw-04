de631215ecede2970b4560f9457935cd50ce58af
TheBearWPL <maksymilian_jarosz@op.pl>
2024-07-23 22:14:16 +0200

    update

--- task-2.js
+++ task-2.js
@@ -1,1 +1,9 @@
 "use strict";
+function calcAverageCalories(days) {
+  if (!Array.isArray(days) || days.length === 0) {
+    return 0;
+  }
+  const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
+  const averageCalories = totalCalories / days.length;
+  return averageCalories;
+}
